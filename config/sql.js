480d28a19a25c1f7e314170f8fd02519a062c284
Rachit Kumar <84663169+rachit3014@users.noreply.github.com>
2026-07-04 20:49:33 +0530

    Comment out database setup queries in sql.js
    Commented out database creation and usage queries.

--- config/sql.js
+++ config/sql.js
@@ -4,6 +4,7 @@ const db=   mysql.createConnection({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT
 });
 
  db.connect(function(error){
@@ -21,24 +22,24 @@ const db=   mysql.createConnection({
     
 
 })
-db.query(
-     `CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`,
-    (error, res)=>{
-        if (error)
-        {
-            console.log(error);
-            return 
-        }
-        console.log('Database created sucessfully or already exists');
-    }
-)
-db.query(`USE ${process.env.DB_NAME}`, (err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+// db.query(
+//      `CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`,
+//     (error, res)=>{
+//         if (error)
+//         {
+//             console.log(error);
+//             return 
+//         }
+//         console.log('Database created sucessfully or already exists');
+//     }
+// )
+// db.query(`USE ${process.env.DB_NAME}`, (err) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
 
-    console.log(`Using database ${process.env.DB_NAME}`);
-});
+//     console.log(`Using database ${process.env.DB_NAME}`);
+// });
 
 module.exports= db;
